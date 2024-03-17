c2895abde24438939f14bef03fec95e89a7a1324
NathanSnail <nathan.titirangi@gmail.com>
2024-03-17 21:10:14 +1300

    op

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import functools
 
 defs = {"<dead_greek>": "<MultiKey> <G>"}
 with open("compose_src","r",encoding="utf-8") as f:
-    c = "# REAL FILE IN COMPOSITION CODE\n"f.read()
+    c = f"# REAL FILE IN COMPOSITION CODE\n{f.read()}"
     v = functools.reduce(lambda a,b: a.replace(b[0], b[1]), defs.items(), c)
     with open("compose_out","w",encoding="utf-8") as o:
         o.write(v)
