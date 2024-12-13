6e00b7aa3aeeae2868bd08c06419f3125a307796
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-13 21:42:48 +1300

    add bang syntax

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import functools
 
 defs = {"<greek>": "<Multi_key> <g>", "<math>": "<Multi_key> <m>"}
 with open("compose_src.XCompose", "r", encoding="utf-8") as f:
-    c = f"# REAL FILE IN COMPOSITION CODE\n{f.read()}"
-    v = functools.reduce(lambda a, b: a.replace(b[0], b[1]), defs.items(), c)
-    with open("compose_out.XCompose", "w", encoding="utf-8") as o:
-        o.write(v)
+    base = f"# REAL FILE IN COMPOSITION CODE\n{f.read()}"
+    remapped = functools.reduce(lambda a, b: a.replace(b[0], b[1]), defs.items(), base)
+    out = ""
+    lines = remapped.split("\n")
+    while len(lines) > 0:
+        first = lines[0]
+        lines = lines[1:]
+        if "<!" not in first:
+            out += first + "\n"
+            continue
+        parts = first.split("<!")
+        s = parts[1].split(">")
+        parts[1] = s[0].upper() + ">" + ">".join(s[1:])
+        lines.append("<".join(parts))
+        parts[1] = s[0].lower() + ">" + ">".join(s[1:])
+        lines.append("<".join(parts))
+
+    with open("compose_out.XCompose", "w", encoding="utf-8") as file:
+        file.write(out)
